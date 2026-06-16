66b79cb2b6b031a2125232d43ea7648fdc2522e1
Nekal Singh Salaria <nekalsingh987@gmail.com>
2026-06-16 10:27:12 +0530

    react learning

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,18 +1,11 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Navabar from "./components/Navabar";
+import Counter from "./Counter";
 
 const App = () => {
   return (
     
-    <BrowserRouter>
-    <Navabar/>
-    <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/about" element={<h1>About</h1>} />
-        <Route path="/contact" element={<h1>Contact</h1>} />
-        <Route path="*" element={<h1>404 Not Found</h1>} />
-    </Routes>
-    </BrowserRouter>
+   <div>
+    <Counter/>
+   </div>
   )
 }
 
---
+++ React/src/Counter.jsx
@@ -0,0 +1,18 @@
+import { useSelector, useDispatch } from 'react-redux';
+  import { increment, decrement, reset } from './counterSlice';
+
+  function Counter() {
+    const count = useSelector(state => state.counter.value);
+    const dispatch = useDispatch();
+
+    return (
+      <div>
+        <h2>Count: {count}</h2>
+        <button onClick={() => dispatch(increment())}>+</button>
+        <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(reset())}>Reset</button>
+      </div>
+    );
+  }
+
+  export default Counter;
