50f1745f3d9f28baa0ae84279c263df96b89dca3
Nekal Singh Salaria <nekalsingh987@gmail.com>
2026-07-15 12:54:44 +0530

    change some notes in event listener and add some notes in learn.txt

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,2 +1,19 @@
-let heading = document.getElementById("main-1");
-heading.style.color = "red";
+const title = document.getElementById("title");
+title.textContent = "DOM Manipulation Project";
+title.innerHTML = "<span>Welcome <b>Students</b></span>";
+title.style.color = "white";
+title.style.background = "blue";
+title.style.padding = "10px";
+const input = document.getElementById("studentName");
+
+const addBtn = document.getElementById("addBtn");
+
+const removeBtn = document.getElementById("removeBtn");
+
+const container = document.getElementById("container");
+
+console.log(title);
+console.log(input);
+console.log(addBtn);
+console.log(removeBtn);
+console.log(container);
