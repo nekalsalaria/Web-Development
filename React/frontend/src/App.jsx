fbedb57ed4e6f0f38b8b932ea92462b65174f486
Nekal Singh Salaria <nekalsingh987@gmail.com>
2026-09-25 10:35:44 +0530

    add React frontend files

--- React/frontend/src/App.jsx
+++ React/frontend/src/App.jsx
@@ -1,16 +1,32 @@
-import Card from './Card.jsx';  
-function App() {
+import {Routes, Route, Link, useParams } from 'react-router-dom';
+
+  function Home() { return <h1>🏠 Home Page</h1>; }
+  function About() { return <h1>📖 About Page</h1>; }
+  function Contact() { return <h1>📞 Contact Page</h1>; }
+
+  function StudentDetail() {
+    const { id } = useParams(); // Read :id from URL
+    return <h2>Student ID: {id}</h2>;
+  }
+
+  function App() {
     return (
-      <div>
-        <Card title="Student Info">
-          <p>Name: Arjun</p>
-          <p>Roll No: 101</p>
-        </Card>
-        <Card title="Course">
-          <p>React JS</p>
-          <button>Enroll</button>
-        </Card>
-      </div>
+      <>
+        <nav>   
+          <Link to="/">Home</Link> |{" "}
+          <Link to="/about">About</Link> |{" "}
+          <Link to="/contact">Contact</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/student/:id" element={<StudentDetail />} />
+          <Route path="*" element={<h1>404 - Page Not Found</h1>} />
+        </Routes>
+      </>
     );
   }
+
 export default App;
